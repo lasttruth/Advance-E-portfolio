364bb678d365d832ed9d471902af4571b76cd8dd
Robin <robindoirin@gmail.com>
2023-11-05 02:19:22 -0500

    added emailjs to modal and added transitions and fuctionality to links and btns

---
+++ app.js
@@ -0,0 +1,36 @@
+// template_r3jmnwd
+// service_uhxbhdw
+// X0sprH3IfwIu2Slsk
+
+function contact(event) {
+  event.preventDefault();
+
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+
+  emailjs.sendForm(
+    "service_uhxbhdw",
+    "template_r3jmnwd",
+    event.target,
+    "X0sprH3IfwIu2Slsk"
+  ).then(() => {
+    loading.classList.remove("modal__overlay--visible");
+    success.classList += " modal__overlay--visible";
+  }).catch(() => {
+    loading.classList.remove("modal__overlay--visible");
+    alert(
+        "the email service is temporarily unvailble. Please contact me directly at robindoirin@gmail.com"
+    );
+  })
+}
+
+let isModalOpen = false
+function toggleModal(){
+    if(isModalOpen){
+        isModalOpen= false;
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open"
+}
